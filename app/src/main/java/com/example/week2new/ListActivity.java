274e980071f0db79fb73dbb7db1bfe6ee6717b85
Loh Wei Ru Natalie <s10243551@connect.np.edu.sg>
2023-06-08 18:22:10 +0800

    week4

--- app/src/main/java/com/example/week2new/ListActivity.java
+++ app/src/main/java/com/example/week2new/ListActivity.java
@@ -1,17 +1,24 @@
 package com.example.week2new;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -20,8 +27,21 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        String name = generateRandom();
+        ArrayList<User> userList = new ArrayList<User>();
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
+        //ArrayList<User> userList = generateUserList(20);
+        generateUserList(userList, 20);
+
+        UserAdapter uAdapter = new UserAdapter(userList);
+
+        LinearLayoutManager uLayoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setLayoutManager(uLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(uAdapter);
+
+        /*
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Profile");
         builder.setMessage("MADness");
@@ -29,7 +49,7 @@ public class ListActivity extends AppCompatActivity {
         builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 Intent intent = new Intent(ListActivity.this, MainActivity.class);
-                intent.putExtra("Key", name);
+                intent.putExtra("Key", );
                 startActivity(intent); //changes to the destination activity
             }
         });
@@ -39,9 +59,8 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
-        ImageView image = findViewById(R.id.imageView3);
-
-        image.setOnClickListener(new OnClickListener()
+        ImageView image = findViewById(R.id.imageView);
+        image.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View image) {
@@ -49,8 +68,113 @@ public class ListActivity extends AppCompatActivity {
                 alert.show();
             }
         });
+*/
     }
-    private String generateRandom() {
+
+    //User View Holder
+    public class UserViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public ImageView profilePicture;
+        public TextView contactName;
+        public TextView messagePreview;
+
+        public UserViewHolder(View itemView) {
+            super(itemView);
+            profilePicture = itemView.findViewById(R.id.imageView);
+            contactName = itemView.findViewById(R.id.textView);
+            messagePreview = itemView.findViewById(R.id.textView2);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int row = getLayoutPosition();
+
+            if (contactName.getText().toString().endsWith("7"))
+            {
+
+            }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(itemView.getContext());
+            builder.setTitle("Profile");
+            builder.setMessage(contactName.getText().toString());
+            builder.setCancelable(true);
+            builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                    intent.putExtra("Key", contactName.getText().toString());
+                    startActivity(intent); //changes to the destination activity
+                }
+            });
+            builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    //action
+                }
+            });
+
+            AlertDialog alert = builder.create();
+            alert.show();
+
+//            ImageView image = findViewById(R.id.imageView);
+//            image.setOnClickListener(new View.OnClickListener()
+//            {
+//                @Override
+//                public void onClick(View image) {
+//                    AlertDialog alert = builder.create();
+//                    alert.show();
+//                }
+//            });
+
+            //Toast.makeText(itemView.getContext(), String.valueOf(row), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public class UserAdapter extends RecyclerView.Adapter<UserViewHolder> {
+        ArrayList<User> userList;
+
+        public UserAdapter(ArrayList<User> data) {
+            this.userList = data;
+            Log.v("MAD", "Size is " + String.valueOf(userList.size()));
+        }
+
+        public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View item = LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.view_holder,
+                    parent,
+                    false);
+            return new UserViewHolder(item);
+        }
+
+        public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+            User user = userList.get(position);
+            holder.contactName.setText(user.getName());
+            holder.messagePreview.setText(user.getDesc());
+
+            Log.v("MAD", user.getName() + " is at " + String.valueOf(position));
+        }
+
+        public int getItemCount() {
+            return userList.size();
+        }
+
+    }
+
+    // method to create userList
+    public ArrayList<User> generateUserList(ArrayList<User> userList, int count) {
+        //ArrayList<User> userList = new ArrayList<>();
+
+        for (int i = 1; i <= count; i++) {
+            User user = new User();
+            user.setId(i);
+            userList.add(user);
+            //Log.v("MAD", user.getName());
+        }
+
+        return userList;
+    }
+}
+
+    /*
+    public String generateRandom() {
         String name = "";
 
         Random randomNum = new Random();
@@ -62,4 +186,20 @@ public class ListActivity extends AppCompatActivity {
 
         return name;
     }
-}
+     */
+
+/*
+    public static void main(String[] args) {
+        List<User> userList = generateUserList(20);
+
+        // Print the details of each user
+        for (User user : userList) {
+            System.out.println("ID: " + user.getId());
+            System.out.println("Name: " + user.getName());
+            System.out.println("Desc: " + user.getDesc());
+            System.out.println("Followed: " + user.isFollowed());
+            System.out.println();
+        }
+    }
+ */
+
--- app/src/main/java/com/example/week2new/User.java
+++ app/src/main/java/com/example/week2new/User.java
@@ -1,36 +1,74 @@
 package com.example.week2new;
 
+import java.util.UUID;
+
 public class User {
     public String name;
-    public String getName(){
+    public String description;
+    public String id;
+    public Boolean followed;
+
+    public User() {
+        this.name = generateRandomName();
+        this.description = generateDescription();
+        this.id = generateUUID();
+        this.followed = generateRandomBoolean();
+    }
+
+    public String getName() {
         return name;
     }
-    public void setName(String newName){
-        this.name = newName;
+
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String description;
-    public String getDesc(){
+    public String getDesc() {
         return description;
     }
-    public void setDesc(String newDesc){
-        this.description = newDesc;
+
+    public void setDesc(String desc) {
+        this.description = desc;
     }
 
-    public Integer id;
-    public Integer getId(){
+    public String getId() {
         return id;
     }
-    public void setId(Integer newId){
-        this.id = newId;
+    public void setId(int id) {
+        this.id = Integer.toString(id);
     }
 
-    public Boolean followed;
-    public Boolean getfollowed(){
+    public boolean isFollowed() {
         return followed;
     }
-    public void setfollowed(Boolean newFollowed){
-        this.followed = newFollowed;
+
+    public void setFollowed(boolean followed) {
+        this.followed = followed;
+    }
+
+    public String generateRandomNumber(int digitsMin, int digitsMax) {
+        int numDigits = (int) (Math.random() * (digitsMax - digitsMin + 1)) + digitsMin;
+        StringBuilder sb = new StringBuilder(numDigits);
+        for (int i = 0; i < numDigits; i++) {
+            int digit = (int) (Math.random() * 10);
+            sb.append(digit);
+        }
+        return sb.toString();
+    }
+    public String generateRandomName(){
+        String prefix = "Name";
+        String randomNumber = generateRandomNumber(9, 10);
+        return prefix + randomNumber;
+    }
+    public String generateDescription(){
+        return generateRandomNumber(9, 10);
+    }
+
+    public String generateUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    public boolean generateRandomBoolean() {
+        return Math.random() < 0.5;
     }
 }
-
